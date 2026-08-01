7face90827b8e4f6786a86f12541a8c7af31018d
jck <jck@shinsegae.com>
2026-08-01 11:45:45 +0900

    ....

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,3 @@
-ssh rocky@121.78.128.86
+ssh -i /Users/jck/WORK/DEV/ms-server.pem rocky@121.78.128.86
 
 
--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 tar -cvf CareerLink.tar .
-scp CareerLink.tar rocky@121.78.128.86:/home/rocky/app/careerlink
+scp -i /Users/jck/WORK/DEV/ms-server.pem CareerLink.tar rocky@121.78.128.86:/home/rocky/app/careerlink
 
 
